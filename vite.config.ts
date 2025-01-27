c6ed81946142017ce49f27362363a46d5d813c81
bleeeeek <mame0009@student.monash.edu>
2025-01-27 10:49:27 +0800

    Update package.json and vite config for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/wifi-network-analyzer/',
+  base: '/network-analysis/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
